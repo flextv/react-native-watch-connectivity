447ea7577d4f566e689ca891010ad80600437dfd
Petr Bela <bela.petr@gmail.com>
2018-02-11 19:30:30 -0700

    import headers compatible with RN 0.40 changes

--- Libraries/RNWatch/WatchBridge.h
+++ Libraries/RNWatch/WatchBridge.h
@@ -1,5 +1,11 @@
-// RNCookieManagerIOS.h
+// import RCTBridgeModule.h
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#elif __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "React/RCTBridgeModule.h"
+#endif
 
 @import WatchConnectivity;
 
